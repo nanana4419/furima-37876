ebb1600f3320015ef40085d58a58aecace2019e3
nanana4419 <warosuwarosuorznatajg@yahoo.co.jp>
2022-05-21 21:20:24 +0900

    消しました

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,6 @@ class User < ApplicationRecord
     validates :last_name, presence: true
     validates :first_name, presence: true
   end
-  # validates :last_name_kana, presence: true
-  # validates :first_name_kana, presence: true
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates :password, format: { with: VALID_PASSWORD_REGEX}
   validates :last_name_kana, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/}
