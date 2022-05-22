4671fc9320719923d9a56a9377bd6d88cc84b4d5
nanana4419 <warosuwarosuorznatajg@yahoo.co.jp>
2022-05-22 22:01:55 +0900

    出品ボタン

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,25 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.order("created_at DESC")
   end
 
   def new
     @item = Item.new
   end
   
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:item_name, :explanation, :user, :category_id, :situation_id, :delivery_charge_id, :area_id, :shipping_day_id, :price)
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,14 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :situation
+  belongs_to :delivery_charge
+  belongs_to :area
+  belongs_to :shipping_day
+
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :situation_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :delivery_charge_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :area_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :shipping_day_id, numericality: { other_than: 1 , message: "can't be blank"} 
 end
