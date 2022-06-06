7b14bf3e1f619a810794b6fc28418442faef4190
nanana4419 <warosuwarosuorznatajg@yahoo.co.jp>
2022-06-06 10:35:46 +0900

    修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -15,7 +15,7 @@ class Item < ApplicationRecord
   validates :delivery_charge_id, numericality: { other_than: 1 , message: "can't be blank"} 
   validates :area_id, numericality: { other_than: 1 , message: "can't be blank"} 
   validates :shipping_day_id, numericality: { other_than: 1 , message: "can't be blank"} 
-  validates :price, numericality: { only_integer: true, greater_than: 299, less_than: 100000000}
+  validates :price, numericality: { only_integer: true, greater_than: 299, less_than: 9999999}
 
   belongs_to :user
   has_one    :record
