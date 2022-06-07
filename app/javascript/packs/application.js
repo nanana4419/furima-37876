70061c521be39ce7e905427d31350de8809fa305
nanana4419 <warosuwarosuorznatajg@yahoo.co.jp>
2022-06-07 18:48:18 +0900

    消した

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,8 +10,6 @@ require("channels")
 require("../card")
 require("../item_price");
 require('../preview')  
-require("../memo")  // 追記する
-
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
